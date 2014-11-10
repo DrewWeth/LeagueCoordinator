660cee9d2e2fca00bdc66664d19e9898d3458819
DrewWeth <dgwetherington@gmail.com>
2014-11-09 20:19:49 -0600

    crash problem fixed

--- app/authorizers/application_authorizer.rb
+++ app/authorizers/application_authorizer.rb
@@ -13,24 +13,5 @@ class ApplicationAuthorizer < Authority::Authorizer
     false
   end
 
-  after_filter :store_location
-
-def store_location
-  # store last url - this is needed for post-login redirect to whatever the user last visited.
-  return unless request.get?
-  if (request.path != "/users/sign_in" &&
-      request.path != "/users/sign_up" &&
-      request.path != "/users/password/new" &&
-      request.path != "/users/password/edit" &&
-      request.path != "/users/confirmation" &&
-      request.path != "/users/sign_out" &&
-      !request.xhr?) # don't store ajax calls
-    session[:previous_url] = request.fullpath
-  end
-end
-
-def after_sign_in_path_for(resource)
-  session[:previous_url] || root_path
-end
 
 end
