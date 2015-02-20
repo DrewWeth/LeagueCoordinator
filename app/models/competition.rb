f2c26e2c67e5ac60de2ecf7184440ce432554667
DrewWeth <dgwetherington@gmail.com>
2015-02-20 02:33:35 -0600

    styling

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -3,14 +3,16 @@ class Competition < ActiveRecord::Base
   has_many :users, :through => :players_in_competitions
   has_many :teams, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 25 }
   validates :at, presence: true
-  validates :location, presence: true
+  validates :location, presence: true, length: { maximum: 100 }
+  validates :user_id, :presence => true
+
+  validates_length_of :description, :maximum => 300, :message=>"Description can only be 300 characters."
 
   belongs_to :user
 
-  validates_length_of :name, :minimum => 3, :maximum => 25, :allow_blank => false
-  validates_length_of :description, :maximum => 300, :allow_blank => true
-  validates_length_of :location, :maximum => 100, :allow_blank => false
+
+
 
 end
