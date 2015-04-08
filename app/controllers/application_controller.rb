1c94e3ef33f674803106d9a66d5cda225481b900
DrewWeth <dgwetherington@gmail.com>
2015-04-08 15:52:38 -0500

    removed all redirecting on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,23 +17,23 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :summoner_name, :phone) }
   end
 
-  def store_location
-    # store last url - this is needed for post-login redirect to whatever the user last visited.
-    return unless request.get?
-    if (request.path != "/users/sign_in" &&
-        request.path != "/users/sign_up" &&
-        request.path != "/users/password/new" &&
-        request.path != "/users/password/edit" &&
-        request.path != "/users/confirmation" &&
-        request.path != "/users/sign_out" &&
-        !request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath
-    end
-  end
-
-  def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
-  end
+  # def store_location
+  #   # store last url - this is needed for post-login redirect to whatever the user last visited.
+  #   return unless request.get?
+  #   if (request.path != "/users/sign_in" &&
+  #       request.path != "/users/sign_up" &&
+  #       request.path != "/users/password/new" &&
+  #       request.path != "/users/password/edit" &&
+  #       request.path != "/users/confirmation" &&
+  #       request.path != "/users/sign_out" &&
+  #       !request.xhr?) # don't store ajax calls
+  #     session[:previous_url] = request.fullpath
+  #   end
+  # end
+  #
+  # def after_sign_in_path_for(resource)
+  #   session[:previous_url] || root_path
+  # end
 
 
   def set_default_params
