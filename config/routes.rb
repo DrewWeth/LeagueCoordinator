49cc215efee0ca539b8c43b47f8e7a8f248ddc2c
DrewWeth <dgwetherington@gmail.com>
2015-09-24 21:57:38 -0500

    redirect for non www

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  constraints subdomain: false do
+    get ':any', to: redirect(subdomain: 'www', path: '/%{any}'), any: /.*/
+  end
+
   get 'home/index'
 
   get 'home/about'
